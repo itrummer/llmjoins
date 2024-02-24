675fcb34ff5df27ef5bd03988a9804b0ee18e86e
itrummer <immanuel.trummer@gmail.com>
2024-02-24 17:03:40 -0500

    Finished code for first simulated benchmark.

---
+++ src/llmjoin/benchmark.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 24, 2024
+
+@author: immanueltrummer
+'''
+from llmjoin.simulator import run_benchmark
+
+
+if __name__ == '__main__':
+    
+    run_benchmark(
+        '500:10500:500', 5000, '30:31:1', 30, 
+        2, '1:2:1', 2, 8192 , 50, 50, 'scale_table_1.csv')
+    run_benchmark(
+        '5000:5001:1', 5000, '50:1050:50', 30, 
+        2, '1:2:1', 2, 8192 , 50, 50, 'scale_tuple_1.csv')
+    run_benchmark(
+        '5000:5001:1', 5000, '30:31:1', 30, 
+        2, '0:1001:50', 2, 8192 , 50, 50, 'scale_output_1.csv')
--- src/llmjoin/simulator.py
+++ src/llmjoin/simulator.py
@@ -48,7 +48,8 @@ def simulate_tuple_join(r1, r2, s1, s2, s3, g, p):
         'tuple_read':total_read, 'tuple_written':total_written}
 
 
-def simulate_block_join(r1, r2, s1, s2, s3, sigma, estimate, g, p, t):
+def simulate_block_join(
+        r1, r2, s1, s2, s3, sigma, estimate, g, p, t, log_prefix):
     """ Simulate block-nested loops join.
     
     Args:
@@ -58,27 +59,140 @@ def simulate_block_join(r1, r2, s1, s2, s3, sigma, estimate, g, p, t):
         s2: size of each tuple in second table.
         s3: size of each output tuple.
         sigma: selectivity of join predicate.
-        estimate: either "precise" or "conservative" selectivity estimate.
+        estimate: selectivity estimate used to calculate block sizes.
         g: relative cost of writing tuples.
         p: size of static part of prompt.
         t: maximal number of tuples processed per model invocation.
+        log_prefix: prefix all logged attributes by this.
     
     Returns:
         A dictionary with simulation results.
     """
-    b1 = math.floor((math.sqrt(s1*s1*s2*s2+s1*s2*s3*sigma*t)-s1*s2)/(s1*s3*sigma))
-    b2 = math.floor((t-b1*s1)/(s2+b1*s3*sigma))
-    nr_invocations = math.ceil(r1/b1) * math.ceil(r2/b2)
+    estimate = max(estimate, 0.0000001)
+    b1 = math.floor(
+        (math.sqrt(s1*s1*s2*s2+s1*s2*s3*estimate*(t-p))-s1*s2)/
+        (s1*s3*estimate))
+    b2 = math.floor(((t-p)-b1*s1)/(s2+b1*s3*estimate))
     prompt_size = p+b1*s1+b2*s2
     output_size = b1*b2*sigma*s3
+    
+    # Check for prompt overflow
+    total_tokens = prompt_size + output_size
+    if total_tokens > t:
+        total_read = prompt_size
+        overflow_tokens = total_tokens - t
+        total_written = output_size - overflow_tokens
+        total_cost = total_read + total_written * g
+        return {
+            f'{log_prefix}_block_invocations':1, 
+            f'{log_prefix}_block_cost':total_cost,
+            f'{log_prefix}_block_read':total_read, 
+            f'{log_prefix}_block_written':total_written,
+            f'{log_prefix}_block_overflow':True}
+    
+    nr_invocations = math.ceil(r1/b1) * math.ceil(r2/b2)
     total_read = prompt_size * nr_invocations
     total_written = output_size * nr_invocations
     total_cost = total_read + total_written * g
     return {
-        f'block_{estimate}_invocations':nr_invocations, 
-        f'block_{estimate}_cost':total_cost,
-        f'block_{estimate}_read':total_read, 
-        f'block_{estimate}_written':total_written}
+        f'{log_prefix}_block_invocations':nr_invocations, 
+        f'{log_prefix}_block_cost':total_cost,
+        f'{log_prefix}_block_read':total_read, 
+        f'{log_prefix}_block_written':total_written,
+        f'{log_prefix}_block_overflow':False}
+
+
+def simulate_adaptive_join(r1, r2, s1, s2, s3, sigma, estimate, g, p, t):
+    """ Simulate adaptive join algorithm.
+    
+    Args:
+        r1: number of rows in first table.
+        r2: number of rows in second table.
+        s1: size of tuples in first table.
+        s2: size of tuples in second table.
+        s3: size of output entries.
+        sigma: actual selectivity of join predicate.
+        estimate: initial estimate for join selectivity (may be too low).
+        g: relative cost of written tokens.
+        p: size of static part of prompt template.
+        t: maximal number of tokens read and generated per LLM invocation.
+    """
+    nr_invocations = 0
+    total_read = 0
+    total_written = 0    
+    total_cost = 0
+    overflow = True
+    
+    while overflow:
+        result = simulate_block_join(
+            r1, r2, s1, s2, s3, sigma, 
+            estimate, g, p, t, '')
+        nr_invocations += result['_block_invocations']
+        total_read += result['_block_read']
+        total_written += result['_block_written']
+        total_cost += result['_block_cost']
+        overflow = result['_block_overflow']
+        estimate *= 4
+    
+    return {
+        f'adaptive_invocations':nr_invocations, 
+        f'adaptive_cost':total_cost,
+        f'adaptive_read':total_read, 
+        f'adaptive_written':total_written}
+
+
+def run_benchmark(
+        r1, r2, s1, s2, s3, sigma_pm, 
+        g, t, p_tuple, p_block, out_file):
+    """ Runs benchmark and writes result to .csv file.
+    
+    Args:
+        r1: range of sizes for first table (format: "start:end:step").
+        r2: size of second table.
+        s1: range of tuple sizes for first table.
+        s2: size of tuple in second table.
+        s3: size of join result tuple.
+        sigma_pm: pro mille selectivity range.
+        g: relative cost per output token.
+        t: maximal number of tokens per LLM invocation.
+        p_tuple: size of static prompt part for tuple join.
+        p_block: size of static prompt part for block join.
+        out_file: name of result output file. 
+    """
+    r1_range = extract_range(r1)
+    s1_range = extract_range(s1)
+    sigma_pm_range = extract_range(sigma_pm)
+    
+    results = []
+    for r1 in r1_range:
+        for s1 in s1_range:
+            for sigma_pm in sigma_pm_range:
+                sigma = 0.001 * sigma_pm
+                result = {
+                    'r1':r1, 'r2':r2, 
+                    's1':s1, 's2':s2, 's3':s3, 
+                    'sigma':sigma, 'g':g, 't':t,
+                    'p_tuple':p_tuple, 'p_block':p_block}
+                result = result | simulate_tuple_join(
+                    r1, r2, s1, s2, s3, 
+                    g, p_tuple)
+                result = result | simulate_block_join(
+                    r1, r2, s1, s2, s3, 
+                    sigma, sigma, 
+                    g, p_block, t, 'informed')
+                result = result | simulate_block_join(
+                    r1, r2, s1, s2, s3, 
+                    sigma, 1, 
+                    g, p_block, t, 'conservative')
+                result = result | simulate_adaptive_join(
+                    r1, r2, s1, s2, s3, 
+                    sigma, sigma/100.0, 
+                    g, p_block, t)
+                
+                results += [result]
+    
+    df = pandas.DataFrame(results)
+    df.to_csv(out_file, index=False)
 
 
 if __name__ == '__main__':
@@ -97,32 +211,7 @@ if __name__ == '__main__':
     parser.add_argument('out_file', type=str, help='Name of result file')
     args = parser.parse_args()
     
-    r1_range = extract_range(args.r1)
-    s1_range = extract_range(args.s1)
-    sigma_pm_range = extract_range(args.sigma_pm)
-    
-    results = []
-    for r1 in r1_range:
-        for s1 in s1_range:
-            for sigma_pm in sigma_pm_range:
-                sigma = 0.001 * sigma_pm
-                result = {
-                    'r1':r1, 'r2':args.r2, 
-                    's1':s1, 's2':args.s2, 's3':args.s3, 
-                    'sigma':sigma, 'g':args.g, 't':args.t,
-                    'p_tuple':args.p_tuple, 'p_block':args.p_block}
-                result = result | simulate_tuple_join(
-                    r1, args.r2, s1, args.s2, args.s3, 
-                    args.g, args.p_tuple)
-                result = result | simulate_block_join(
-                    r1, args.r2, s1, args.s2, args.s3, 
-                    sigma, 'precise', 
-                    args.g, args.p_block, args.t)
-                result = result | simulate_block_join(
-                    r1, args.r2, s1, args.s2, args.s3, 
-                    1, 'conservative', 
-                    args.g, args.p_block, args.t)
-                results += [result]
-    
-    df = pandas.DataFrame(results)
-    df.to_csv(args.out_file, index=False)
+    run_benchmark(
+        args.r1, args.r2, args.s1, args.s2, args.s3, 
+        args.sigma_pm, args.g, args.t, 
+        args.p_tuple, args.p_block, args.out_file)
